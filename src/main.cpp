1941f966eefbda1a7eb90b8a6623b524ce953ea7
Iyad Hamid <iyad.hamid@gmail.com>
2023-09-21 11:31:27 -0500

    Wrote field output of function to file

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,35 @@ template <typename T, int N>
 T function(vec<T, N> x) {
 	T out = 1;
 	for (auto i : x)
-		out *= sin(i);
+		out *= std::sin(i);
 	return out;
 }
 
-
-int main() {
-	auto c_min = splat<2>(0.0), c_max = splat<2>(1.0);
-	 
+int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) {
+	auto c_min = splat<2>(0.0);
+	auto c_max = splat<2>(1.0);
+	
 	auto c_range = c_max - c_min;
 	auto h = 0.1;
+
 	auto total_points = vec_cast<int>(c_range / h) + splat<2>(1);
+	auto size = prod(total_points);
 
-	std::vector<double> points(prod(total_points));
+	std::vector<double> field;
+	field.reserve(size);
 
-	std::cout << points.size() << std::endl;
+	
+	// Should replace with md-span
+	for (int i = 0; i < size; ++i) {
+		auto ioffset = vec_cast<double>(unflatten(i, total_points));
+		auto pos = mul(div(ioffset, vec_cast<double>(total_points)), c_range) + c_min;
+		field.push_back(function(pos));
+	}
+
+	std::ofstream fout("out.txt");
+	for (auto p : field)
+		fout << p << "\n";
+
 	return 0;
 }
 
